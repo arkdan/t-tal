0f1945c47eeea8812b345e4ed842887cba8815a0
ark dan <arkdan@icloud.com>
2022-09-02 09:06:39 +0300

    initial

---
+++ t-talUITests/t_talUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  t_talUITestsLaunchTests.swift
+//  t-talUITests
+//
+//  Created by mac on 9/2/22.
+//
+
+import XCTest
+
+class t_talUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
